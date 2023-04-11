7e15549a64a1e38159314742566ccd6152af653b
Fran Linde Blazquez <franlindebl@gmail.com>
2023-04-11 18:37:41 +0200

    Added POST and search by name

--- index.js
+++ index.js
@@ -41,6 +41,40 @@ router.get("/user/:id", (req, res) => {
     .catch((error) => res.status(500).json(error));
 });
 
+router.get("/user/name/:name", async (req, res) => {
+  const name = req.params.name;
+
+  try {
+    // Busqueda exacta
+    // const user = await User.find({ firstName: name });
+
+    const user = await User.find({ firstName: new RegExp("^" + name.toLowerCase(), "i") });
+    if (user?.length) {
+      res.json(user);
+    } else {
+      res.status(404).json([]);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Endpoint de creación de usuarios
+router.post("/user", async (req, res) => {
+  try {
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phone: req.body.phone,
+    });
+
+    const createdUser = await user.save();
+    return res.status(201).json(createdUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 server.use("/", router);
 server.listen(PORT, () => {
   console.log(`Server levantado en el puerto ${PORT}`);
